d15f77dd3c33d075636b01487d164b1e9acd69b3
Ryan Birmingham <birm@rbirm.us>
2021-11-09 22:20:52 -0500

    add web service, simplify path names

---
+++ Web/README.md
@@ -0,0 +1,4 @@
+# Rivers Web Service
+
+Java 17, Spring Boot 2.5.6.
+Deps: Spring Web and MariaDB Driver
---
+++ Web/src/test/java/us/rbirm/rivers/RiversApplicationTests.java
@@ -0,0 +1,13 @@
+package us.rbirm.rivers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
