6f7a98e4ef6edc174329535f7f775c962f69dd69
Ryan Birmingham <birm@rbirm.us>
2021-12-16 11:34:00 -0500

    skeleton components

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GridComponent } from './grid/grid.component';
+import { SiteComponent } from './site/site.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GridComponent,
+    SiteComponent,
+    LineChartComponent
   ],
   imports: [
     BrowserModule
