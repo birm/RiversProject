d8a09c0b70b8763484d72682372f6b6d2bf3a858
Ryan Birmingham <birm@rbirm.us>
2022-01-13 10:42:13 -0500

    vaguely 'prettier' ui kinda

--- Frontend/src/app/site/site.component.ts
+++ Frontend/src/app/site/site.component.ts
@@ -6,7 +6,7 @@ import {Observation} from '../observation'
 @Component({
   selector: 'app-site',
   templateUrl: './site.component.html',
-  styleUrls: ['./site.component.css']
+  styleUrls: ['./site.component.css'],
 })
 export class SiteComponent implements OnInit {
 
